d5d720e36172f12778073184f8c8fb5ae6eacb2f
Mikołaj Lubiak <lubiak@proton.me>
2023-09-08 16:30:37 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,62 @@
+import http.server
+from http import HTTPStatus
+import json
+import random
+import string
+import hashlib
+
+challenge = ''.join(random.choices(string.ascii_lowercase, k=64))
+difficulty = 5
+
+# Define the handler for GET and POST requests
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Set response status code and headers
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Read and serve an HTML file
+        with open('index.html', 'r') as file:
+            html_content = file.read()
+
+        html_content = html_content.replace('EXAMPLE', challenge)
+        html_content = html_content.replace('DIFFICULTY', str(difficulty))
+
+        self.wfile.write(html_content.encode())
+
+    def do_POST(self):
+        # Set response status code and headers
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Read and process POST data as JSON
+        try:
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode('utf-8')
+            post_params = json.loads(post_data)
+
+            # Process the POST request and send a response
+            msg = challenge + str(post_params['nonce'])
+            hex_hash = hashlib.sha512(msg.encode()).hexdigest()
+            if (
+                    post_params['challenge'] == challenge and
+                    post_params['hash'].startswith('0'*difficulty) and
+                    hex_hash == post_params['hash']
+                    ):
+                response_message = 'good'
+            else:
+                response_message = 'bad'
+
+            self.wfile.write(response_message.encode('utf-8'))
+        except Exception as e:
+            error_message = f'Error processing POST request: {str(e)}'
+            self.wfile.write(error_message.encode('utf-8'))
+
+# Create an HTTP server that listens on port 8000
+httpd = http.server.HTTPServer(('localhost', 8000), MyHandler)
+print('Server started on http://localhost:8000')
+
+# Start the server
+httpd.serve_forever()
